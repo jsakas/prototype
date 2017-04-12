d1d9089241c17b9d78f5f7aded67611bfdf03f2d
Jon Sakas <jon.sakas@beatport.com>
2017-04-12 10:21:10 -0600

    add error html to installation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='prototype',
- 	version='1.0.2',
+ 	version='1.0.3',
 	description='A static site generator.',
 	url='http://github.com/jsakas/prototype',
 	author='Jon Sakas',
@@ -26,6 +26,7 @@ setup(
         	'template/*/*.*',
         	'template/*/*/*.*',
         	'template/*/*/*/*.*',
+        	'html/*.*',
         ]
     },
 )
